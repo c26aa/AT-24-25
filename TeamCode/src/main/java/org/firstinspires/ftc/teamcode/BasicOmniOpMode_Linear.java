478b6d70b79756af619345b46a02c67f2f0ba71d
Aaron <c26aa@dalton.org>
2024-11-14 18:58:18 -0500

    updated2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
@@ -7,16 +7,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
+@TeleOp(name="Atomic Tele-Op", group="Linear OpMode")
 //@Disabled
 public class BasicOmniOpMode_Linear extends LinearOpMode {
 
     private double currentPosition = 0.45;
-    private double currentPosition1 = 0.4;// Start the servo at the middle position
+    private double currentPosition1 = 0.4; // Start the servo at the middle position
     private static final double CHANGE_AMOUNT = 0.02;
+    private final double claw_start = 0.5;
+    private double claw_target = 0.5;
+    private static final double claw_speed = 0.01;
 
     // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
+    private final ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
@@ -34,7 +37,6 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
 
     // Add variables for slow mode
     private boolean slowMode = false;
-    private boolean previousBState = false;
     private final double SLOW_MODE_FACTOR = 0.5; // Adjust this value to change the slow mode speed
 
     @Override
@@ -88,6 +90,13 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
             if (gamepad1.x)
             {
                 slowMode = !slowMode;
+            }
+            if(gamepad1.left_bumper){
+bucket.setPosition(.6);
+            }
+
+            if(gamepad1.right_bumper){
+bucket.setPosition(.99);
             }
 
             double axial   = -gamepad1.left_stick_y;
@@ -145,19 +154,72 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
                 slide_right.setPosition(currentPosition1);
                 sleep(100);// 100 ms delay each time
             }
-
-            if (gamepad2.x) {
-
+            double lift_power = 0.2;//95
+        int lift_max_right = 3000;//not true
+        int lift_min_right = 3000;//not true
+            int lift_max_left = 3000;//not true
+            int lift_min_left = 3000;//not true
+            if (gamepad2.dpad_up) {//scissor lift
+                lift_left.setPower(lift_power);
+//                lift_left.setTargetPosition(lift_max);
+//                lift_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                lift_right.setPower(lift_power);
+//                lift_right.setTargetPosition(lift_max);
+//                lift_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
-            if (gamepad2.y) {
-
+            if (gamepad2.dpad_down) {
+                lift_left.setPower(-lift_power);
+//                lift_left.setTargetPosition(lift_min);
+//                lift_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                lift_right.setPower(-lift_power);
+//                lift_right.setTargetPosition(lift_min);
+//                lift_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
 
+            if(gamepad2.y) {//back position
+                bar_left.setPosition(1.0);
+                bar_right.setPosition(1.0);
+                left_right_hinge.setPosition(0.6);
+                up_down_hinge.setPosition(0.27);
+                claw.setPosition(0.5);
+            }
+
+            if(gamepad2.b) {//pick up from top
+//                bar_left.setPosition(null);
+//                bar_right.setPosition(null);
+//                left_right_hinge.setPosition(0.6);
+//                up_down_hinge.setPosition(0.35);
+//                claw.setPosition(0.5);
+            }
+
+            if(gamepad2.a) {// pick up from side
+//                bar_left.setPosition(null);
+//                bar_right.setPosition(null);
+//                left_right_hinge.setPosition(0.6);
+//                up_down_hinge.setPosition(0.42);
+//                claw.setPosition(0.5);
+            }
+
+            double leftStickX = gamepad1.left_stick_x;
+            claw_target = 0.5 + Math.abs((leftStickX * 0.1));
+            if (gamepad2.left_stick_x > 0 || gamepad2.left_stick_x < 0 ){
+                claw.setPosition(claw_target);
+            }
+            if (gamepad2.right_stick_x > 0 ){
+                left_right_hinge.setPosition(0.55);
+            }
+            else if (gamepad2.right_stick_x < 0 ) {
+                left_right_hinge.setPosition(0.65);
+            }
+            int lift_encoder_value = lift_left.getCurrentPosition();
+            int lift_encoder_value2 = lift_right.getCurrentPosition();
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Slow Mode", slowMode ? "Enabled" : "Disabled");
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("encoder_value_left", lift_encoder_value);
+            telemetry.addData("encoder_value_right", lift_encoder_value2);
             telemetry.update();
         }
     }
