f5f47a6a3b90c86388a8c1c0089d3cd0f7ab93fd
mountaindewsalesman <mountaindewsalesman@gmail.com>
2025-02-11 08:35:13 -0500

    created test file
    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPosTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPosTesting.java
@@ -20,7 +20,7 @@ package org.firstinspires.ftc.teamcode;
  *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *   SOFTWARE.
- */
+ */ 
 
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
